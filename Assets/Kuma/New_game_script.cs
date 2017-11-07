9a6f6d7d0673712f2dd34c29afdb2d79d882a3c8
yuugon <pizaman-u@ezweb.ne.jp>
2017-11-07 17:58:07 +0900

    start,animation追加

---
+++ Assets/Kuma/New_game_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class New_game_script : MonoBehaviour {
+
+    public void ButtonPush()
+    {
+        SceneManager.LoadScene("VRtest キャラメイク");
+    }
+    // Use this for initialization
+  
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
