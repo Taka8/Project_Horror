19b848940e9e03cde8280d7d903b030e10c9e526
d0Y0b <exa7538315@gmail.com>
2017-11-09 11:33:04 +0900

    アイテム

--- Assets/Yoshitake/Assets/Paper/Materials/PaperCrane.cs
+++ Assets/Yoshitake/Assets/Paper/Materials/PaperCrane.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PaperCrane : MonoBehaviour
 {
+    public GameObject Enemy;
 
     // トリガーとの接触時に呼ばれるコールバック
     void OnTriggerEnter(Collider hit)
     {
+
         //接触対象はPlayerタグですか?
         if (hit.CompareTag("Player"))
         {
             //このコンポーネントを持つGameObjectを破棄する
             Destroy(gameObject);
+
+            //Enemyを非表示
+            Enemy.SetActive(false);
+
+            Debug.Log("hello");
         }
+
     }
 }
