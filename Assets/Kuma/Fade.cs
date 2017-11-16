e85f28bb8fe2322a09687bb8792159d2f8cc2934
yuugon <pizaman-u@ezweb.ne.jp>
2017-11-16 13:42:03 +0900

    Goal設定

---
+++ Assets/Kuma/Fade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Fade : MonoBehaviour {
+    public Image image;
+	// Use this for initialization
+	void Start () {
+        image.color = Color.white;
+        image.DOFade(0, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
