ca5cecfe386ed9c2d398068735579badb6d1461d
kazushimorita <moririn.neko2koban@gmail.com>
2017-11-17 16:47:44 +0900

    更新

--- Assets/Morita/NewBehaviourScript1.cs
+++ Assets/Morita/NewBehaviourScript1.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class NewBehaviourScript1 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // トリガーとの接触時に呼ばれるコールバック
+    void OnTriggerEnter(Collider hit)
+    {
+        //接触対象はPlayerタグですか?
+        if (hit.CompareTag("Player"))
+        {
+            //このコンポーネントを持つGameObjectを破棄する
+            Destroy(gameObject);
+        }
+    }
+
 }
